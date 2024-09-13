ff031000748c61ffda92713d31aec2d6e3ff75bd
rahul patil <>
2024-09-13 08:22:17 +0530

    Assignment 4: 5th Question

--- assignment4.py
+++ assignment4.py
@@ -104,12 +104,29 @@ list_of_dictionaries
  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 
 5.Write a Python program to filter a list of integers using Lambda. 
+ 
  Original list of integers: 
  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+ 
  Even numbers from the said list: 
  [2, 4, 6, 8, 10] 
+ 
  Odd numbers from the said list: 
  [1, 3, 5, 7, 9] 
+
+ # :: Solution ::
+ print("Original list of integers:  ")
+
+list_of_integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list_of_integers
+
+print("Even numbers from the said list: ")
+list(filter(lambda ls: (ls%2 == 0), list_of_integers))
+
+print("Odd numbers from the said list:")
+list(filter(lambda ls: (ls%2 != 0), list_of_integers))
+
+ --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 
 6.Write a Python program to square and cube every number in a given list of integers using Lambda. 
  Original list of integers: 
