dd944657c648ac22b17b0682e8b93aa6a269112d
rahul patil <>
2024-09-18 05:55:40 +0530

    Add: Assignment 3: and 1:

---
+++ assignment1.py
@@ -0,0 +1,90 @@
+Assignment: 1 
+
+1. What is the output of the following code?  
+nums = set([1,1,2,3,3,3,4,4])  
+print(len(nums))   
+
+# :: Solution ::
+4
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
+
+2. What will be the output? d = {"john":40, "peter":45} print(list(d.keys()))
+
+# :: Solution ::
+['john', 'peter']
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
+
+3.  A website requires a user to input username and password to register. Write a program to check the validity of password given by user. 
+Following are the criteria for checking password:   
+
+At least 1 letter between [a-z] 
+At least 1 number between [0-9]  
+At least 1 letter between [A-Z] 
+At least 1 character from [$#@] 
+Minimum length of transaction password: 6  
+Maximum length of transaction password: 12 
+
+
+# :: Solution ::
+
+import re
+
+def validate_password(password):
+    if (6 <= len(password) <= 12
+        and re.search("[a-z]", password)
+        and re.search("[A-Z]", password)
+        and re.search("[0-9]", password)
+        and re.search("[$#@]", password)):
+        return True
+    else:
+        return False
+
+password = input("Enter your password: ")
+if validate_password(password):
+    print("Password is valid.")
+else:
+    print("Password is invalid.")
+
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
+
+3. Write a for loop that prints all elements of a list and their position in the list.      
+  a = [4,7,3,2,5,9]  
+
+  # :: Solution ::
+  ['john', 'peter']
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---   
+
+ Please write a program which accepts a string from console and print the characters that have even indexes. 
+Example: If the following string is given as input to the program: H1e2l3l4o5w6o7r8l9d 
+Then, the output of the program should be: Helloworld 
+
+Please write a program which accepts a string from console and print it in reverse order.
+
+ Example: If the following string is given as input to the program:  rise to vote sir 
+Then, the output of the program should be: ris etov ot esir
+
+Please write a program which count and print the numbers of each character in a string input by console. 
+Example: If the following string is given as input to the program: abcdefgabc 
+Then, the output of the program should be: 
+a,2
+c,2
+b,2
+e,1
+d,1
+g,1
+f,1
+
+With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list whose elements are intersection of the above given lists. 
+With a given list [12,24,35,24,88,120,155,88,120,155], write a program to print this list after removing all duplicate values with original order reserved. 
+ By using list comprehension, please write a program to print the list after removing the value 24 in [12,24,35,24,88,120,155]. 
+ By using list comprehension, please write a program to print the list after removing the 0th,4th,5th numbers in [12,24,35,70,88,120,155]. 
+. By using list comprehension, please write a program to print the list after removing delete numbers which are divisible by 5 and 7 in [12,24,35,70,88,120,155]. 
+Please write a program to randomly generate a list with 5 numbers, which are divisible by 5 and 7 , between 1 and 1000 inclusive. 
+Write a program to compute 1/2+2/3+3/4+...+n/n+1 with a given n input by console (n>0). 
+
+Example: If the following n is given as input to the program: 5 
+Then, the output of the program should be: 3.55  
+
+15.
---
+++ assignment3.py
@@ -0,0 +1,12 @@
+
+Assignment 3:
+
+Assignment- OOP
+
+1. Write a Python program to create a class representing a stack data structure. Include methods for pushing and popping elements.
+
+2. Write a Python program to create a class representing a linked list data structure. Include methods for displaying linked list data, inserting and deleting nodes.
+
+3. Write a Python program to create a class representing a shopping cart. Include methods for adding and removing items, and calculating the total price.
+
+4. Write a Python program to create a class representing a stack data structure. Include methods for pushing, popping and displaying elements.
