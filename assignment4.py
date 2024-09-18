bc86fd675562b142195bd0448132c775c608757d
rahul patil <>
2024-09-18 05:39:34 +0530

    Assignment 4: 11th Question

--- assignment4.py
+++ assignment4.py
@@ -293,8 +293,24 @@ print(f"Fibonacci series up to 9: {fibonacci_series(9)}")  # [0, 1, 1, 2, 3, 5, 
 
 
 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---  
+
 11.Write a Python program to find the intersection of two given arrays using Lambda. 
  Original arrays: 
  [1, 2, 3, 5, 7, 8, 9, 10] 
  [1, 2, 4, 8, 9] 
  Intersection of the said arrays: [1, 2, 8, 9]
+
+# :: Solution ::
+
+# Original arrays
+arr1 = [1, 2, 3, 5, 7, 8, 9, 10]
+arr2 = [1, 2, 4, 8, 9]
+
+# Lambda function to find the intersection
+intersection = list(filter(lambda x: x in arr2, arr1))
+
+# Output the result
+print("Original arrays:")
+print(arr1)
+print(arr2)
+print("Intersection of the said arrays:", intersection)
